32b61234fe46b5dca5e3fe419376557a3e241577
Vita Koval <mayevit@hotmail.com>
2022-09-15 19:09:01 +0300

    fixed/tasck-1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from "./Profile.module.css";
+import css from './Profile.module.css';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
@@ -30,19 +30,11 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
 };
 
 Profile.propTypes = {
-  user: PropTypes.arrayOf(
-    PropTypes.exact({
-      username: PropTypes.string.isRequired,
-      tag: PropTypes.string.isRequired,
-      location: PropTypes.string.isRequired,
-      avatar: PropTypes.string.isRequired,
-      stats: {
-        followers: PropTypes.number.isRequired,
-        views: PropTypes.number.isRequired,
-        likes: PropTypes.number.isRequired,
-      },
-    })
-  ),
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number),
 };
 
 // username — имя пользователя
