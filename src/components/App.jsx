27edce8ae02ad2429df87d95626a539b005734b8
Vita Koval <mayevit@hotmail.com>
2022-09-16 13:54:22 +0300

    Task - 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import { Profile } from './Profile/Profile';
 import user from 'user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from 'data.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from 'friends.json';
 
 export const App = () => {
   return (
@@ -22,10 +24,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics
-        title="Upload stats"
-        stats={data}
-      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
